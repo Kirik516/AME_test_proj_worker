cde43a85b7111ffc19c5af4b31ec6e7297ea1d41
Kirill <37420066+Kirik516@users.noreply.github.com>
2019-08-25 02:10:08 +0300

    Класс коэфицаентного потока доработан до рабочего состояния; текущее значение оборнуто в атомик; добавлена переменная контроля работы потока

--- coef_thread.cpp
+++ coef_thread.cpp
@@ -1,6 +1,42 @@
 #include "coef_thread.h"
 
-CoefThread::CoefThread(float startVal, float endVal)
+void CoefThread::flowCoef()
 {
+    float val;
+    while (this->isRunning)
+    {
+        std::chrono::duration<float> time =
+                            std::chrono::system_clock::now() - this->startTime;
+        this->currentVal = std::sinf(time.count() * this->freq)
+                            * this->range + this->startVal + this->range/2.0;
+//        std::cout << "value generated: " << this->currentVal << std::endl;
+//        std::cout << "asd" << std::endl;
+//        this->currentVal
+        std::this_thread::sleep_for(std::chrono::milliseconds(30));
+    }
+}
 
+CoefThread::CoefThread(float startVal, float endVal)
+    : isRunning(0)
+    , freq(M_PI)
+    , startVal(startVal)
+    , range(endVal - startVal)
+    , currentVal(startVal)
+{}
+
+void CoefThread::setFreq(float freq)
+{
+    this->freq = freq;
+}
+
+float CoefThread::getVal()
+{
+    return this->currentVal.load();
+}
+
+void CoefThread::run()
+{
+    this->isRunning = true;
+    this->startTime = std::chrono::system_clock::now();
+    currenThread = std::thread(&CoefThread::flowCoef, this);
 }
--- coef_thread.h
+++ coef_thread.h
@@ -1,10 +1,32 @@
+#include <chrono>
+#include <atomic>
 #include <thread>
+#include <cmath>
+#include <iostream>
 
 class CoefThread
 {
-	// sin frequency
-	float freq;
+    bool isRunning;
+
+    // sin frequency in sec
+    float freq;
+
+    // read only fields
+    float startVal;
+    float range;
+
+    // write fields
+    std::atomic<float> currentVal;
+    std::chrono::time_point<std::chrono::system_clock> startTime;
+
+    // thread obj
+    std::thread currenThread;
+
+    void flowCoef();
 
 public:
-	CoefThread(float startVal, float endVal);
+    CoefThread(float startVal, float endVal);
+    void setFreq(float freq);
+    float getVal();
+    void run();
 };
