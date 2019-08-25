62a0dcaec72c17e0dd64dded36201523ebe12700
Kirill <37420066+Kirik516@users.noreply.github.com>
2019-08-25 19:22:26 +0300

    Доабвлена возможность установки частоты и останоки и запуска потоков

--- Window.cpp
+++ Window.cpp
@@ -12,15 +12,110 @@ TForm1 *Form1;
 __fastcall TForm1::TForm1(TComponent* Owner)
 	: TForm(Owner)
     , coefA(3.0, 7.0)
+    , coefB(1.0, 3.0)
+    , coefC(1.0, 10.0)
 {
+    this->freqMap[EditFreqA] = &this->coefA;
+    this->freqMap[EditFreqB] = &this->coefB;
+    this->freqMap[EditFreqC] = &this->coefC;
+
+    this->stopMap[ButtonA] = &this->coefA;
+    this->stopMap[ButtonB] = &this->coefB;
+    this->stopMap[ButtonC] = &this->coefC;
+
+    this->valMap[ButtonA] = this->EditA;
+    this->valMap[ButtonB] = this->EditB;
+    this->valMap[ButtonC] = this->EditC;
+
     this->coefA.run();
-    std::cout << "programm started" << std::endl;
+    this->coefB.run();
+    this->coefC.run();
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm1::Timer1Timer(TObject *Sender)
 {
-    std::string valA = std::to_string(this->coefA.getVal());
-    this->EditA->Text = valA.c_str();
+    // setting current values to edits
+    std::string val = std::to_string(this->coefA.getVal());
+    this->EditA->Text = val.c_str();
+
+    val = std::to_string(this->coefB.getVal());
+    this->EditB->Text = val.c_str();
+
+    val = std::to_string(this->coefC.getVal());
+    this->EditC->Text = val.c_str();
+}
+//---------------------------------------------------------------------------
+int TForm1::setFreq(TEdit *edit, CoefThread *coefThread)
+{
+    float freqVal = edit->Text.ToDouble();
+    if (!std::isfinite(freqVal))
+    {
+        return 1;
+    }
+
+    coefThread->setFreq(freqVal);
+    return 0;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::EditFreqKeyPress(TObject *Sender, System::WideChar &Key)
+
+{
+    System::WideChar enterKey = 13;
+    if (Key != enterKey)
+    {
+        return;
+    }
+
+    TEdit *edit = dynamic_cast<TEdit*>(Sender);
+    if (!edit)
+    {
+        return;
+    }
+
+    if (!this->freqMap.count(edit))
+    {
+        return;
+    }
+
+    if (this->setFreq(edit, this->freqMap[edit]))
+    {
+        edit->Text = "Bad";
+    }
+}
+//---------------------------------------------------------------------------
+int TForm1::stopStartThread(TButton *button, CoefThread *coefThread)
+{
+    if (coefThread->isRunning())
+    {
+        coefThread->stop();
+        this->valMap[button]->Enabled = true;
+        button->Caption = "Run";
+        return 0;
+    }
+
+    coefThread->run();
+    this->valMap[button]->Enabled = false;
+    button->Caption = "Stop";
+
+    return 0;
+}
+void __fastcall TForm1::ButtonTrCntClick(TObject *Sender)
+{
+    TButton *button = dynamic_cast<TButton*>(Sender);
+    if (!button)
+    {
+        return;
+    }
+
+    if (!this->stopMap.count(button) || !this->valMap.count(button))
+    {
+        return;
+    }
+
+    if (this->stopStartThread(button, this->stopMap[button]))
+    {
+        this->valMap[button]->Text = "Bad";
+    }
 }
 //---------------------------------------------------------------------------
 
--- Window.h
+++ Window.h
@@ -10,6 +10,7 @@
 #include <Vcl.ExtCtrls.hpp>
 //---------------------------------------------------------------------------
 #include <string>
+#include <map>
 
 #include "coef_thread.h"
 //---------------------------------------------------------------------------
@@ -36,9 +37,27 @@ __published:	// IDE-managed Components
     TEdit *EditFreqB;
     TEdit *EditFreqC;
     void __fastcall Timer1Timer(TObject *Sender);
+    void __fastcall EditFreqKeyPress(TObject *Sender, System::WideChar &Key);
+    void __fastcall ButtonTrCntClick(TObject *Sender);
 
 private:	// User declarations
+    // threads
     CoefThread coefA;
+    CoefThread coefB;
+    CoefThread coefC;
+
+    // for freq set
+    std::map<TEdit*, CoefThread*> freqMap;
+
+    // for thread stop/start
+    std::map<TButton*, CoefThread*> stopMap;
+    std::map<TButton*, TEdit*> valMap;
+
+    // 0 - on success; 1 - on fail
+    // set freq from edit value to thread
+    int setFreq(TEdit *edit, CoefThread *coefThread);
+    // stop/start thread
+    int stopStartThread(TButton *button, CoefThread *coefThread);
 
 public:		// User declarations
 	__fastcall TForm1(TComponent* Owner);
