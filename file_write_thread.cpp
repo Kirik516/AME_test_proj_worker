3eaa88ee42d8db0fc233c99d1f82ccb2c188e360
Kirill <37420066+Kirik516@users.noreply.github.com>
2019-09-08 00:21:27 +0300

    Добавлен файл исходного кода для класса записи коэффициентов в файл

---
+++ file_write_thread.cpp
@@ -0,0 +1,20 @@
+#include "file_write_thread.h"
+
+void FileWriteThread::fileWrite(float coefA, float coefB, float coefC)
+{
+
+}
+//---------------------------------------------------------------------------
+
+void FileWriteThread::run()
+{
+    this->fileWrite(this->coefA, this->coefB, this->coefC);
+}
+//---------------------------------------------------------------------------
+
+void FileWriteThread::setCoefs(float coefA, float coefB, float coefC)
+{
+
+}
+//---------------------------------------------------------------------------
+
